bae9f5b37a16d9454fd845f22902516fcecd4a4c
Alireza <alirezakhoshghalb@ymail.com>
2018-04-02 08:32:25 +0430

    Requirements

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-pardakht',
-    version='1.2',
+    version='1.3',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',  # example license
@@ -31,9 +31,9 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
+	install_requires = [
+		'django-extensions==1.9.8',
+		'zeep==2.5.0'
+	]
 )
 
-dependencies = [
-	'django-extensions==1.9.8',
-	'zeep==2.5.0'
-]
