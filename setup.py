fd84a70c243427baed2eefb26756b0b14d9921f0
Alireza <alirezakhoshghalb@ymail.com>
2018-04-02 06:44:06 +0430

    Add readme.rst

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-pardakht',
-    version='1.1',
+    version='1.2',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',  # example license
